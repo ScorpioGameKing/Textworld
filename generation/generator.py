6ae830e916380f5a0bbd6ccb3e73b57be75b4159
Dalton Edmsiten <daltonedmisten@gmail.com>
2025-02-13 09:25:11 -0600

    Updated noist generation library to speed up generation

--- generation/generator.py
+++ generation/generator.py
@@ -1,35 +1,38 @@
 from time import gmtime, strftime
 import numpy as np
 from opensimplex import OpenSimplex
+from noise import snoise3
 
 from generation.map import TextworldMap
 from models import Size, Tile, Coords
 from database import TileDatabase
 
 class TextworldGenerator():
+    
+    __seed: int
+    
     def __init__(self, seed:int = int(strftime("%Y%m%d%H%M%S", gmtime()))):
-        self.seed = seed
-        self.height_noise = np.array([OpenSimplex(self.seed), OpenSimplex(self.seed * 2), OpenSimplex(self.seed * 4), OpenSimplex(self.seed * 8)])
+        self.__seed = seed % np.random.default_rng(seed).integers(10000, 100000)
 
         
 
     # Default Map Generation
     def get_chunk(self, size:Size[int], chunk_coords: Coords) -> TextworldMap:
-        scale = 0.5 * 0.0625
+        scale = (0.5 * 0.0625) / 2
         chunk = TextworldMap(size)
 
         
         with TileDatabase() as db:
-        
             for y in range(size.height):
                 
+                noise_y = ((y + (chunk_coords.y * size.height)) * scale) + self.__seed
                 for x in range(size.width):
-                    noise_val =(self.height_noise[0].noise2((x + (chunk_coords.x * size.width)) * scale, (y + (chunk_coords.y * size.height)) * scale) +
-                    (self.height_noise[1].noise2((x + (chunk_coords.x * size.width)) * scale, (y + (chunk_coords.y * size.height)) * scale * 0.5)) +
-                    (self.height_noise[2].noise2((x + (chunk_coords.x * size.width)) * scale, (y + (chunk_coords.y * size.height)) * scale * 0.25)) +
-                    (self.height_noise[3].noise2((x + (chunk_coords.x * size.width)) * scale, (y + (chunk_coords.y * size.height)) * scale * 0.125))) / 2
-                    db_tile = db.get_tile_by_noise(noise_val)
-
+                    noise_val = 0
+                    noise_x = ((x + (chunk_coords.x * size.width)) * scale) + self.__seed
+                    for z in range(4):
+                        noise_val += snoise3(noise_x, noise_y, z + self.__seed) / ( z + 1)
+                    
+                    db_tile = db.get_tile_by_noise(noise_val / 2)
                     if not db_tile:
                         db_tile = db.get_tile('X')
                     chunk[x,y] = db_tile
--- generation/world.py
+++ generation/world.py
@@ -21,45 +21,46 @@ class TextworldWorld():
         self.lock = threading.Lock()
         
     def __generate_chunk(self, coords: Coords):
-        logging.debug(f"Thread for Chunk [{coords.x}, {coords.y}] started")
+        logging.debug(f"Generation for Chunk [{coords.x}, {coords.y}] started")
         
         chunk = self.generator.get_chunk(self.chunk_size, coords)
         with self.lock:
             self.__chunks[coords] = chunk
-            
-        logging.debug(f"Thread for Chunk [{coords.x}, {coords.y}] closing")
+        logging.debug(f"Generation for Chunk [{coords.x}, {coords.y}] finished")
         
-    
-    def generate_map(self, progress_callback: Callable[[],None] = (lambda x:logging.debug(f"Progress: {math.floor(x*100)}%"))):
+    def __generate_chunks(self):
+        logging.debug('Chunk generation started')
+        
         half_height = round(self.chunk_count.height / 2) or 1
         half_width = round(self.chunk_count.width / 2) or 1
-        
         logging.debug(f'Height values {0-half_height} - {half_height}')
         logging.debug(f'Width values {0 - half_width} - {half_width}')
-        
-        threads: list[threading.Thread] = []
         for x in range(0 - half_height, half_height ):
-            for y in range(0 - half_width, half_width):
-                threads.append(
-                    threading.Thread(None, self.__generate_chunk, f'Chunk {x} {y}', (Coords(x,y),), daemon=True)
-                )
+             for y in range(0 - half_width, half_width):
+                self.__generate_chunk(Coords(x,y))
                 
-        for t in threads:
-            t.start()
-            
+        logging.debug('Chunk generation finished')
+        
+        
+                 
+    
+    def generate_map(self, progress_callback: Callable[[],None] = (lambda x:logging.debug(f"Progress: {math.floor(x*100)}%"))):
+
+        t = threading.Thread(target=self.__generate_chunks)
+        t.start()
+        
         def progress():
             
             logging.debug("Progress thread started")
-            while any([t.is_alive() for t in threads]):
+            while t.is_alive():
                 _progress =  len(self.__chunks.keys()) / self.chunk_count.area()
                 progress_callback(_progress)
                 sleep(3)
-
-            
+        
         progress_thread = threading.Thread(target=progress, name='progress thread' )
         progress_thread.start()
         progress_thread.join()
-
+        
     def __getitem__(self, coords: tuple[int,int]) -> np.typing.NDArray:
         return self.__chunks[Coords(*coords)]
     
