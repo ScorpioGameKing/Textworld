f58a6561e457a5ec703141f258db109095131287
Dalton Edmsiten <daltonedmisten@gmail.com>
2025-02-17 20:23:32 -0600

    added full db init to genasave

--- genasave.py
+++ genasave.py
@@ -1,6 +1,9 @@
 from generation import TextworldWorld
-from database import WorldDatabase
+from database import WorldDatabase, Database, Color as ColorQueries, World as WorldQueries, Tile as TileQueries
 from models import Size
+
+with Database() as db:
+    db.init_db(ColorQueries.INIT, ColorQueries.INIT, WorldQueries.INIT, TileQueries.INIT, TileQueries.FILL)
 
 world = TextworldWorld(chunk_count=Size(5,5), chunk_size=Size(150,150))
 world.generate_map()
