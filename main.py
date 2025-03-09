c9a2fd7be31264d4f57af8623b9599d53cc798c3
Dalton Edmsiten <daltonedmisten@gmail.com>
2025-03-08 21:04:39 -0600

    Forgot main

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from kivy.config import Config
+
+
+Config.set('kivy', 'log_level', 'error')
+Config.write()
+
 from kivy.app import App
 from engine.ui_manager import TextworldUIManager
 from database import Database
 from database import Color, World, Tile
+import logger
+
+logger.trace("Logger configured")
 
 # The main kivy App, build the required parts, schedule the loops and return
 class TextworldApp(App):
@@ -17,6 +26,10 @@ class TextworldApp(App):
 
 # Config Write settings to be moved
 if __name__ == '__main__':
+    logger.trace('Initializing Application')
+
     with Database() as db:
         db.init_db(Color.INIT, Color.FILL, Tile.INIT, Tile.FILL, World.INIT)
+    logger.Config.set_log_level(logger.Level.INFO)
+    logger.debug('check if level updated')
     TextworldApp().run()
